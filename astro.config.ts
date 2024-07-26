d676e63be02e63a62fee94da452f111331386ad1
ideaexplorer233 <66181555+ideaexplorer233@users.noreply.github.com>
2024-07-26 12:23:29 +0800

    feat: initialize repository

---
+++ astro.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      wrap: true,
+      transformers: []
+    }
+  }
+});
---
+++ src/siteConfig.ts
@@ -0,0 +1,4 @@
+export default {
+    siteName: "Blog",
+    siteDescription: "No description.",
+}
