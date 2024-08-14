12da690654441ae32370d97a680bd621de033452
ideaexplorer233 <66181555+ideaexplorer233@users.noreply.github.com>
2024-08-14 18:32:20 +0800

    refactor: dark mode button

--- src/components/DarkModeBtn.svelte
+++ src/components/DarkModeBtn.svelte
@@ -1,28 +1,31 @@
 <script lang="ts">
     let currentTheme = window.localStorage.getItem("theme");
+    const body = document.querySelector("body")!;
     switch (currentTheme) {
         case "dark":
-            document.querySelector("body")?.classList.add("dark");
+            body.classList.add("dark");
         case "light":
-            document.querySelector("body")?.classList.add("light");
+            body.classList.add("light");
     }
     function handleThemeSwitch(event: MouseEvent) {
+        // Enable transition if switch
+        body.classList.add("auto");
         switch (currentTheme) {
             case null:
                 currentTheme = "dark";
                 window.localStorage.setItem("theme", "dark");
-                document.querySelector("body")?.classList.add("dark");
+                body.classList.add("dark");
                 break;
             case "dark":
                 currentTheme = "light";
                 window.localStorage.setItem("theme", "light");
-                document.querySelector("body")?.classList.remove("dark");
-                document.querySelector("body")?.classList.add("light");
+                body.classList.remove("dark");
+                body.classList.add("light");
                 break;
             default:
                 currentTheme = null;
                 window.localStorage.removeItem("theme");
-                document.querySelector("body")?.classList.remove("light");
+                body.classList.remove("light");
         }
     }
 
@@ -31,13 +34,13 @@
 
 <li><span class={styles.separation}>/</span></li>
 <li>
-<button on:click={handleThemeSwitch}>
-    {#if currentTheme === "dark"}
-        Dark
-    {:else if currentTheme === "light"}
-        Light
-    {:else}
-        Auto
-    {/if}
-</button>
+    <button on:click={handleThemeSwitch}>
+        {#if currentTheme === "dark"}
+            Dark
+        {:else if currentTheme === "light"}
+            Light
+        {:else}
+            Auto
+        {/if}
+    </button>
 </li>
